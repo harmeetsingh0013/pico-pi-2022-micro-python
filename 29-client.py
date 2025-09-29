999273142e513f3e65ade5c6cba0ed72ad7404fe
Harmeet Singh <harmeet.singh@autoscout24.com>
2025-09-29 23:52:59 +0200

    add next lessons

---
+++ 29-client.py
@@ -0,0 +1,13 @@
+import socket
+
+serverAddress = ('192.168.178.122', 2222)
+bufferSize = 1024
+UDPClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+while True:
+  cmd = input('Select Color RED/GREEN/BLUE? ')
+  cmdEncoded = cmd.encode('utf-8')
+  UDPClient.sendto(cmdEncoded, serverAddress)
+  data, address = UDPClient.recvfrom(bufferSize)
+  dataDecoded = data.decode('utf-8')
+  print('Response: ', dataDecoded)
